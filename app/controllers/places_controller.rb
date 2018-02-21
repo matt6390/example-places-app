1fcb758532eae536496e1d4ed3c12da0b22aab24
matt6390 <matthewstone639@gmail.com>
2018-02-20 21:31:21 -0600

    Done for the day

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,12 +2,12 @@ class PlacesController < ApplicationController
   def index
     @place = Place.all
 
-    render json: place.as_json
+    render json: @place.as_json
   end
 
   def create
     @place = Place.create(
-                          name: params[:name],)
+                          name: params[:name],
                           address: params[:address]
                           )
     @place.save
